538d7152017d388578ce64a4cd5196c34d104f83
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-07-30 10:16:06 -0300

    Estrutura visual - Setup do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+      <h1>Hello World</h1>
+   </ThemeProvider>
   )
 }
-
-export default App
---
+++ src/styles/global.ts
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${props => props.theme['green-500']};
+  }
+
+  body {
+    background-color: ${props => props.theme['gray-800']};
+    color: ${props => props.theme['gray-100']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font: 400 1rem Roboto, sans-serif;
+  }
+`;
