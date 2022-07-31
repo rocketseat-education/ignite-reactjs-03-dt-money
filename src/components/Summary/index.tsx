6616bda21392ba3be5b9d2ba0098c48dc10217c4
Elias Gabriel <eliasgabrielcf@gmail.com>
2022-07-31 15:19:57 -0300

    Conexão com a API - Criando hook para o resumo

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,30 +1,10 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
-import { useContext } from 'react';
-import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useSummary } from '../../hooks/useSummary';
 import { priceFormatter } from '../../utils/formatter';
 import { SummaryCard, SummaryContainer } from './styles';
 
 export function Summary() {
-  const { transactions } = useContext(TransactionsContext);
-
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,27 @@
+import { useContext } from 'react';
+import { TransactionsContext } from '../contexts/TransactionsContext'
+
+export function useSummary() {
+  const { transactions } = useContext(TransactionsContext);
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.price;
+        acc.total += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+        acc.total -= transaction.price;
+      }
+
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    },
+  );
+
+  return summary;
+}
